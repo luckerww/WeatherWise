6fa39a9633946bdba0fe0eddd6b1de33dc91bc05
luckerww <luckerww@gmail.com>
2024-07-01 11:20:24 -0500

    Initial Commit

---
+++ WeatherWise/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  WeatherWise
+//
+//  Created by 吴魏 on 7/1/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ WeatherWise/WeatherWiseApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherWiseApp.swift
+//  WeatherWise
+//
+//  Created by 吴魏 on 7/1/24.
+//
+
+import SwiftUI
+
+@main
+struct WeatherWiseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
